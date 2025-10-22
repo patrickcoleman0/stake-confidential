f9a95cb8f7ca501a79fa51be667f602c9da19c35
lucasrowe8 <etjgysqy8286@hotmail.com>
2025-10-22 13:37:17 +0800

    fix

--- home/src/config/contracts.ts
+++ home/src/config/contracts.ts
@@ -205,7 +205,7 @@ export const TOKEN_CONTRACTS = [
     name: 'Confidential ETH',
     symbol: 'cETH',
     decimals: 6,
-    address: '0x5FbDB2315678afecb367f032d93F642f64180aa3',
+    address: CONFIDENTIAL_ETH_ADDRESS,
     abi: [
       {
         "inputs": [],
@@ -843,7 +843,7 @@ export const TOKEN_CONTRACTS = [
     name: 'Confidential USDT',
     symbol: 'cUSDT',
     decimals: 6,
-    address: '0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512',
+    address: CONFIDENTIAL_USDT_ADDRESS,
     abi: [
       {
         "inputs": [],
@@ -1481,7 +1481,7 @@ export const TOKEN_CONTRACTS = [
     name: 'Confidential Zama',
     symbol: 'cZAMA',
     decimals: 6,
-    address: '0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0',
+    address: CONFIDENTIAL_ZAMA_ADDRESS,
     abi: [
       {
         "inputs": [],
